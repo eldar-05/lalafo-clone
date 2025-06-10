3d329367304576e7ac5c657b9f465da6536820d2
eldar-05 <eldarzh123@gmail.com>
2025-06-11 00:59:57 +0600

    first commit

---
+++ src/test/java/com/example/lalafo/LalafoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lalafo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LalafoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
